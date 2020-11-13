e7b2c0ab3c2e0519ca9daaa3c6c97b4bf25b570e
Debasish Sarker <debasishsarker36@gmail.com>
2020-11-13 11:55:39 +0600

    created the boilerplate for all the necessary components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,78 +1,36 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
 
-import {
-  MenuItem,
-  FormControl,
-  Select,
-} from "@material-ui/core";
+import Header from './components/Header/Header';
+import Card from './components/Card/Card';
+import Map from './components/Map/Map';
+import Chart from './components/Chart/Chart';
+import Graph from './components/Graph/Graph'
+
+
 
 function App() {
-  // state
-
-  const[countries, setCountries] = useState([]);
-  const[country, setCountry] = useState('Worldwide');
-
-  useEffect( () => {
-    const getCountriesData = async () => {
-      await fetch("https://disease.sh/v3/covid-19/countries")
-      .then((response) => response.json())
-      .then((data) => {
-        const countries = data.map((country) => ({
-          name: country.country,
-          value:country.countryInfo.iso2,
-
-        }));
-        setCountries(countries);
-      });
-    };
-
-    getCountriesData();
-
-  }, []);
-
-  function onCountrySelect(event) {
-    const countryCode = event.target.value;
-
-    setCountry(countryCode);
-  }
-
-
-
+ 
   return (
     <div className="app">
-      
-      {/* Header */}
-      
-      <div className="appHeader">
-      <h1>Covid-19 Tracker</h1>
 
-        <FormControl className="appDropdown">
-          <Select variant="outlined" onChange={onCountrySelect} value={country}>
+      <div className="appLeft">
+        <Header/>
+        
+        <div className="Cards">
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
 
-            <MenuItem value="Worldwide">Worldwide</MenuItem>
-
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-
-          </Select>
-        </FormControl>
+        <Map/>
       </div>
 
+      <div className="appRight">
+        <Chart/>
+        <Graph/>
+      </div>
 
-
-      
-      {/* Title + dropdown */}
-
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
-
-      {/* Tables */}
-      {/* Graphs */}
-
-      {/* Map */}
 
     </div>
   );
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import './Header.css'
+
+
+
+
+function Header(){
+    const[countries, setCountries] = useState([]);
+    const[country, setCountry] = useState('Worldwide');
+
+    useEffect( () => {
+        const getCountriesData = async () => {
+        await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+            const countries = data.map((country) => ({
+            name: country.country,
+            value:country.countryInfo.iso2,
+
+            }));
+            setCountries(countries);
+        });
+        };
+        getCountriesData();
+    }, []);
+
+  function onCountrySelect(event) {
+    const countryCode = event.target.value;
+
+    setCountry(countryCode);
+  }
+
+   
+    return(
+        <div className="appHeader">
+            <h1>Covid-19 Tracker</h1>
+
+            <FormControl className="appDropdown">
+
+                <Select variant="outlined" onChange={onCountrySelect} value={country}>
+                    
+                    <MenuItem value="Worldwide">Worldwide</MenuItem>
+                    {countries.map((country) => (
+                    <MenuItem value={country.value}>{country.name}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+      </div>
+    )
+}
+
+export default Header
